ac4b4cd2fe7093ff1f3d71a45598db79e14d785d
ruandabin <864849501@qq.com>
2018-07-04 22:28:23 +0800

    quick sort

--- algorithms/src/main/java/top/ruandb/sort/QuickSort.java
+++ algorithms/src/main/java/top/ruandb/sort/QuickSort.java
@@ -28,40 +28,57 @@ public class QuickSort {
 		int j = right;
 		int temp;
 		while(i < j ) {
-			while(i <= j && base <= array[j]) {
+			while(i < j && base <= array[j]) {
 				j--;
+			}
+			//System.out.println(j);
+			if(i == j){
+				temp = array[j];
+				array[j] = base;
+				array[left] = temp;
+				break;
 			}
 			while(i < j && base >= array[i]) {
 				i++;
 			}
+			//System.out.println(i);
 			//ArrayUtil.printArray(array);
-			System.out.println(array[i]+"****"+array[j]);
-			if(i == j) {
+			//System.out.println(array[i]+"****"+array[j]);
+			
+			if(i == j){
 				temp = array[j];
 				array[j] = base;
 				array[left] = temp;
-			}else {
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				j--;
-				i++;
-			}	
+				break;
+			}
 			
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;					
 		}
-		
 		return i;
 	}
 	
 	public static void main(String[] args) {
-		//int[] array = ArrayUtil.generateRandomArray(10, 0, 20);
-		int[] array = {10, 10, 0 ,12 ,14 ,13 ,14 ,17, 7 ,1};
+		int[] array = ArrayUtil.generateRandomArray(1000000, 0, 1000000);
+		//int[] array = {5, 9 ,10 ,17 ,7, 10, 14 ,9 ,9 ,10};
+		//int[] array = {7 ,1, 0 ,10};
+		
+		
 		QuickSort qs = new QuickSort();
-		ArrayUtil.printArray(array);
-		//ArrayUtil.printArray(qs.quickSort(array));
-		qs.divide(array, 0, array.length-1);
+//		ArrayUtil.printArray(array);
+//		ArrayUtil.printArray(qs.quickSort(array));
+		
+		
+		//qs.divide(array, 0, array.length-1);
 		//qs.sort(array, 0, array.length-1);
-		ArrayUtil.printArray(array);
+		//ArrayUtil.printArray(array);
+		
+		
+		long startTime = System.currentTimeMillis();
+		qs.quickSort(array);
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime + "ms");
 		
 	}
 }
